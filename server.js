9b2b163ebca2f49dfe4e1a294f12d867e5c3b812
RistaGear <89870535+RistaGear@users.noreply.github.com>
2021-09-28 21:31:49 -0500

    Third try

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require ('express');
 const app = express();
 const path = require('path');
 
-app.use(express.statiic(__dirname + '/dist'));
+app.use(express.statiic(__dirname + '/dist/angular-tour-of-heroes'));
 
 app.listen(process.env.PORT || 5000);
 
 //PathLocationStrategy
 
 app.get('/*', function( req,res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/angular-tour-of-heroes/index.html'));
 })
 
 console.log('Console listenig!');
